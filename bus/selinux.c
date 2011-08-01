70fb675a108dbf660d384150429c9af085b0c48e
Steve Grubb <sgrubb@redhat.com>
2011-08-01 14:28:27 -0400

    capng: Drop supplemental groups when switching to daemon user
    This patch ensures the daemon process doesn't inherit any supplemental
    groups for the root user from an administrator login via an init
    script.
    
    This is only an issue for pre-systemd systems.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=726953

--- bus/selinux.c
+++ bus/selinux.c
@@ -1055,7 +1055,7 @@ _dbus_change_to_daemon_user  (const char    *user,
       capng_clear (CAPNG_SELECT_BOTH);
       capng_update (CAPNG_ADD, CAPNG_EFFECTIVE | CAPNG_PERMITTED,
                     CAP_AUDIT_WRITE);
-      rc = capng_change_id (uid, gid, 0);
+      rc = capng_change_id (uid, gid, CAPNG_DROP_SUPP_GRP);
       if (rc)
         {
           switch (rc) {
