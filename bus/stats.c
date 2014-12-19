eec885de3b4b9559a2f28be7c17bf21ca8d2382f
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-12-19 18:51:04 +0000

    Hardening: only accept Stats function calls at the canonical object path
    These function calls are not a privilege escalation risk like
    UpdateActivationEnvironment, but they might provide sensitive
    information or be enhanced to provide sensitive information
    in future, so the default system.conf locks them down to root-only.
    Apply the same canonical-object-path hardening as for
    UpdateActivationEnvironment.
    
    We do not apply the uid check here because they are less dangerous
    than UpdateActivationEnvironment, and because the ability to unlock
    these function calls for specific uids is a documented configuration
    for developers.
    
    Reviewed-by: Thiago Macieira <thiago@kde.org>
    [added missing #include; extended commit message -smcv]

--- bus/stats.c
+++ bus/stats.c
@@ -29,6 +29,7 @@
 #include <dbus/dbus-connection-internal.h>
 
 #include "connection.h"
+#include "driver.h"
 #include "services.h"
 #include "utils.h"
 
@@ -48,6 +49,9 @@ bus_stats_handle_get_stats (DBusConnection *connection,
   dbus_uint32_t in_use, in_free_list, allocated;
 
   _DBUS_ASSERT_ERROR_IS_CLEAR (error);
+
+  if (!bus_driver_check_message_is_for_us (message, error))
+    return FALSE;
 
   context = bus_transaction_get_context (transaction);
   connections = bus_context_get_connections (context);
@@ -130,6 +134,9 @@ bus_stats_handle_get_connection_stats (DBusConnection *caller_connection,
   DBusConnection *stats_connection;
 
   _DBUS_ASSERT_ERROR_IS_CLEAR (error);
+
+  if (!bus_driver_check_message_is_for_us (message, error))
+    return FALSE;
 
   registry = bus_connection_get_registry (caller_connection);
 
